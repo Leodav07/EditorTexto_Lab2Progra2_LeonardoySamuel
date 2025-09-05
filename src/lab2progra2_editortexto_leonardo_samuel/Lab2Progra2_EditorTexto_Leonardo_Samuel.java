d98e977666801e6764e9861b380e6467a4db3c00
Leonardo López <lopezvelasquezleonardodavid@gmail.com>
2025-09-05 08:15:32 -0600

    primer commit

---
+++ src/lab2progra2_editortexto_leonardo_samuel/Lab2Progra2_EditorTexto_Leonardo_Samuel.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2progra2_editortexto_leonardo_samuel;
+
+/**
+ *
+ * @author hnleo
+ */
+public class Lab2Progra2_EditorTexto_Leonardo_Samuel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
