2c24eb558651d623773253f26b5bf93d4747884e
Leonardo López <lopezvelasquezleonardodavid@gmail.com>
2025-09-05 09:53:11 -0600

    Gestor formato clase

--- src/FileManager/GestorFormato.java
+++ src/FileManager/GestorFormato.java
@@ -6,6 +6,8 @@ package FileManager;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JTextPane;
 import javax.swing.text.Style;
@@ -20,7 +22,7 @@ public class GestorFormato {
     StyledDocument doc;
     Style st;
     
-    public void aplicarFormato(JTextPane pane, String fuente, int tamanio, int estilo, Color color){
+    public void formatoSeleccion(JTextPane pane, String fuente, int tamanio, int estilo, Color color){
         int in = pane.getSelectionStart();
         int fin = pane.getSelectionEnd();
         
@@ -34,5 +36,24 @@ public class GestorFormato {
         StyleConstants.setUnderline(st, (estilo & 4)!=0);
         StyleConstants.setForeground(st, color);
         doc.setCharacterAttributes(in, fin-in, st, false);
+    }
+    
+    public List<FormatoTexto> getFormatos(JTextPane pane){
+        List<FormatoTexto> formats = new ArrayList<>();
+        doc = pane.getStyledDocument();
+        return formats;
+    }
+    
+    public void aplicarFormato(JTextPane pane, List<FormatoTexto> formatos){
+        doc = pane.getStyledDocument();
+        for (FormatoTexto f : formatos) {
+            st = doc.addStyle("AppliedStyle", null);
+             StyleConstants.setFontFamily(st, f.getFontFamily());
+             StyleConstants.setFontSize(st, f.getFontSize());
+             StyleConstants.setBold(st, (f.getFontStyle() & Font.BOLD) != 0);
+            StyleConstants.setItalic(st, (f.getFontStyle() & Font.ITALIC) != 0); 
+            StyleConstants.setForeground(st, f.getColor());
+            doc.setCharacterAttributes(f.getInicio(), f.getFin() - f.getInicio(), st, false);
+        }
     }
 }
